c19555d147ef1374e4fe56280975f8375cee9baa
Jakub Nalewajk <jaqubowskyy@gmail.com>
2025-04-08 22:02:20 +0200

    refactor: try again...

--- src/Version.tsx
+++ src/Version.tsx
@@ -1,7 +1,9 @@
 export const Version = () => {
+  const environment = import.meta.env.VITE_ENVIRONMENT;
+
   return (
     <div className="fixed bottom-4 right-4 px-3 py-1 bg-gray-700 text-white text-xs rounded-full shadow-md">
-      {import.meta.env.VITE_ENVIRONMENT} - {__APP_VERSION__}
+      {environment} - {__APP_VERSION__}
     </div>
   );
 };
