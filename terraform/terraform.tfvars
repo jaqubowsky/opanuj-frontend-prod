b765d564de8360ed7e2b6f45fb879a812cd11f9c
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2025-04-13 13:21:00 +0200

    feat: delete ec2 from configuration (#14)

--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,4 +1,2 @@
-ami            = "ami-0ef32de3e8ab0640e"
-type           = "t2.micro"
 s3_bucket_name = "opanujfrontend-prod"
 environment    = "Production"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,3 @@
-variable "ami" {
-  description = "The AMI ID to use for the EC2 instance"
-  type        = string
-}
-
-variable "type" {
-  description = "The instance type for the EC2 instance"
-  type        = string
-  default     = "t2.micro"
-}
-
 variable "s3_bucket_name" {
   description = "The name for the S3 bucket (must be globally unique)"
   type        = string
